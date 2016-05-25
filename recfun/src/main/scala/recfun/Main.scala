c08d58320560b9347050df2d7f280946de2c3439
Henry Neeb <hneeb@stanford.edu>
2016-05-24 22:46:51 -0700

    completed E1 - Pascal

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -14,8 +14,12 @@ object Main {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
-
+  def pascal(c: Int, r: Int): Int = 
+    if (c == 0 || c == r) {
+      return 1
+    } else {
+      return pascal(c - 1, r - 1) + pascal(c, r - 1)
+    }
   /**
    * Exercise 2
    */
